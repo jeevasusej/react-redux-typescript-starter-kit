55b322b624e0889b226ad46a5d1d2d9ad9c2630a
Jeevananandan J <jeevasusej@gmail.com>
2019-12-17 23:21:58 +0530

    relative path for demo

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-redux-typescript-starter-kit/blob/master/docs/precache-manifest.503b631cb5708edb747a78ad5ccbfc50.js"
+  "/jeevasusej/react-redux-typescript-starter-kit/blob/master/docs/precache-manifest.c1b16c37084db887689e5f589b9130cd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-typescript-starter-kit/blob/master/docs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jeevasusej/react-redux-typescript-starter-kit/blob/master/docs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
